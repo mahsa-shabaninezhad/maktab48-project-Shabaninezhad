a0982c89f87869895589ba7aae184e217e33859e
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-07-09 18:29:49 +0430

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { Switch, Route } from "react-router-dom";
+import { AdminPanelRoute, StoreRoute } from "./components/Routes";
+import LoginPage from "./pages/LoginPage";
+import BasketPage from "./pages/BasketPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import ProductPage from "./pages/ProductPage";
+import ProductsPage from "./pages/ProductsPage";
+import LandingPage from "./pages/LandingPage";
+import AdminPanelProductPage from "./pages/AdminPanelProductPage";
+import AdminPanelQuantityPage from "./pages/AdminPanelQuantityPage";
+import AdminPanelOrdersPage from "./pages/AdminPanelOrdersPage";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+
 function App() {
 
   return (
     <div className="App">
+      <Switch>
+        <StoreRoute exact path='/' component={LandingPage}/>
+        <StoreRoute exact path='/card' component={BasketPage}/>
+        <StoreRoute exact path='/checkout' component={CheckoutPage}/>
+        <StoreRoute exact path='/product/:id' component={ProductPage}/>
+        <StoreRoute exact path='/products/:brand' component={ProductsPage}/>
+        <Route exact path="/login" component={LoginPage}/>
+        <AdminPanelRoute exact path="/adminPanel/products" component={AdminPanelProductPage}/>
+        <AdminPanelRoute exact path="/adminPanel/price&inventory" component={AdminPanelQuantityPage}/>
+        <AdminPanelRoute exact path="/adminPanel/orders" component={AdminPanelOrdersPage}/>
+      </Switch>
       
+      <ToastContainer/>
+
     </div>
   );
 }
