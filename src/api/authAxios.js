8d4827f4f270d9f35f46a71191fb783cffae14f5
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-07-09 23:45:23 +0430

    add axios instance for login api call

---
+++ src/api/authAxios.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+//using axios instance for login api call
+const login = axios.create({
+    baseURL:'https://reqres.in',
+    headers: { "content-type": "application/json" }
+})
+
+//using axios interceptor for showing notifications to user after each call
+login.interceptors.response.use(function (response) {
+    if(response.status === 200 || response.status === 201){
+        localStorage.setItem("token", response.data.token);
+        toast.success('خوش آمدید')
+    }
+    return response
+
+}, function (error) {
+    if (error.response) {
+        window.location.reload()
+        toast.error(error.response.data.error)
+    }
+
+    return Promise.reject(error)
+})
+
+export default login
