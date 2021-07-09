37d4f0d69f3f451b0dcb79b1a7b8f11cccb10c06
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-07-09 18:12:07 +0430

    creating custom route including proper layout

---
+++ src/components/Routes.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Route, Redirect } from 'react-router-dom'
+import AdminPanelLayout from './AdminPanelLayout'
+import StoreLayout from './StoreLayout'
+
+
+export const AdminPanelRoute = ({component: Component, ...rest}) => {
+    
+    const isAdminLogedIn = useSelector(state => state.admin.isLogedIn)
+
+    return ( 
+        <Route {...rest}
+            render={
+                props => {
+                    if(isAdminLogedIn){
+                        return (<AdminPanelLayout>
+                                    <Component {...props}/>
+                                </AdminPanelLayout>)
+                    } else{
+                        return (
+                        <Redirect to={{
+                            pathname:"/login",
+                            state:{
+                                from: props.location,
+                            }
+                        }}/>)
+                    }
+                }
+            }
+
+        />
+    )
+}
+
+
+export const StoreRoute = ({component: Component, ...rest}) => {
+    
+    return ( 
+        <Route {...rest}
+            render={
+                props => <StoreLayout>
+                            <Component {...props}/>
+                        </StoreLayout>
+            }
+
+        />
+    )
+}
+
