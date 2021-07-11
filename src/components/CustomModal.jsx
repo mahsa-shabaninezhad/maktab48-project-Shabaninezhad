31aad446a6d76bac3a1327b093eebaf812e3f2df
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-07-11 15:32:18 +0430

    create a custom modal

---
+++ src/components/CustomModal.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import Modal from '@material-ui/core/Modal';
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/styles';
+import { useDispatch, useSelector } from 'react-redux';
+import { Paper, Typography } from '@material-ui/core';
+import { closeModal} from '../store/actions/modalActions'
+import CancelIcon from '@material-ui/icons/Cancel';
+import Icon from './Icon'
+
+
+const useStyle = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+    width: props => props.width || '500px',
+    height: props => props.height || '450px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    padding: props => props.padding || theme.spacing(4)
+  },
+  header: {
+    display: 'flex', 
+    justifyContent: 'space-between', 
+    width: '100%'
+  }
+}))
+
+const CustomModal = (props) => {
+    const classes = useStyle(props)
+
+    const dispatch = useDispatch()
+    const {isModalShowing, modalContent, modalHeader} = useSelector(state => state.modal)
+
+    const handleCloseModal = (e) => {
+    //  e.stopPropagation()
+      dispatch(closeModal())
+    }
+
+    return (
+        <Modal
+            open={isModalShowing}
+            onClose={!isModalShowing}
+            aria-labelledby="modal-title"
+        >
+            <Paper  className={classes.paper} elevation={3}>
+              <div className={classes.header}>
+                    <Typography id='modal-title' variant='h6' component='h3'>
+                      {modalHeader}
+                    </Typography>
+                    <Icon component={CancelIcon} margin='0' onClick={handleCloseModal}/>
+              </div>
+                {modalContent}
+            </Paper>
+        </Modal>
+    )
+}
+
+CustomModal.propTypes = {
+  children: PropTypes.element
+}
+
+export default CustomModal
