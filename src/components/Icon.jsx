f87f695e0da36d71d33cb097ae8ed18f89689223
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-07-11 15:15:04 +0430

    creat custome icon component

---
+++ src/components/Icon.jsx
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+
+
+const useStyle = makeStyles(theme => ({
+    icon:{
+        cursor: 'pointer',
+        color: props => props.color || theme.palette.primary.main,
+        margin: props => props.margin || theme.spacing(1),
+        transition: 'transfom .2s',
+        '&:hover':{
+            transform: 'scale(1.1)'
+        }
+    }
+}))
+
+const Icon = ({component: Component, ...others}) => {
+    const classes = useStyle(others)
+    return <Component className={classes.icon} {...others}/>
+}
+
+export default Icon
