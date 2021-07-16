872cdc91f1ca2329fc8099ef5a95358fdfb0392d
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-07-16 20:46:56 +0430

    add axios instance for orders

---
+++ src/api/ordersAxios.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+//using axios instance for login api call
+const ordersAxios = axios.create({
+    baseURL:'http://localhost:5000/orders',
+})
+
+//using axios interceptor for showing notifications to user after each call
+ordersAxios.interceptors.response.use(function (response) {
+    if(response.status === 200 || response.status === 201){
+        switch (response.config.method) {
+            case 'put':
+                toast.success('تغییرات با موفقیت اعمال شد.')
+                break;      
+            default:
+                break;
+        }
+    }
+    return response
+
+}, function (error) {
+    if (error.response) {
+        toast.error(error.response.data.error)
+    }
+
+    return Promise.reject(error)
+})
+
+export default ordersAxios
