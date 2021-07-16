536028f5bde209119e124187f61032ec2f3e9067
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-07-16 20:45:23 +0430

    add orderReducer to rootReducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from "redux";
 import { adminReducer } from './adminReducer'
 import { modalReducer } from "./modalReducer";
 import { productsReducer } from "./productsReducer";
+import { loadingReducer } from "./loadingReducer";
+import { ordersReducer } from "./ordersReducer";
 
 export const rootReducer = combineReducers({
     admin: adminReducer,
     products: productsReducer,
-    modal: modalReducer
+    modal: modalReducer,
+    orders: ordersReducer,
+    
 })
