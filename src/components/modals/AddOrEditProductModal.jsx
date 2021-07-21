703dedb4a409ac2dcded5b4a115c1c819be48f99
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-07-21 21:33:39 +0430

    add categories details modal

--- src/components/modals/AddOrEditProductModal.jsx
+++ src/components/modals/AddOrEditProductModal.jsx
@@ -3,9 +3,13 @@ import { Button, TextField, MenuItem, InputBase } from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 import useForm from '../../hooks/useForm'
 import Form from '../Form'
-
+import ModalContainer from './ModalContainer'
 import { addAProduct, editAProduct } from '../../store/actions/productAtions'
 import { handleUploadingImage } from '../../utils/uploadImage'
+import PhoneDetails from './PhoneDetails'
+import LaptopDetails from './LaptopDetails'
+import CameraDetails from './CameraDetails'
+import { useMediaQuery } from '@material-ui/core'
 
 const categories = [
     {
@@ -24,33 +28,46 @@ const categories = [
 
 
 const AddOrEditProductModal = ({neadEditProduct}) => {
-    const [image, setImage] = useState(null)
     const dispatch = useDispatch()
+    const phones = useMediaQuery('(max-width: 450px')
+
+    const [image, setImage] = useState(neadEditProduct?.image || null)
+
+    //open modal for adding details of each category product
+    const [openModal, setOpenModal] = useState(false)
+    //storing data of modal
+    const [details, setDetails] = useState(neadEditProduct?.properties || null)
+
+    //--------------------------------INITIALIZE USEFORM HOOK----------------------------------
 
     //initial state according to edit mode or add mode
     const initialValues = neadEditProduct
     ?{
         title: neadEditProduct.title,
+        model: neadEditProduct.model,
         category: neadEditProduct.category,
         brand: neadEditProduct.brand,
         description: neadEditProduct.description
     } 
     :{
         title: '',
+        model: '',
         category: 'گوشی موبایل',
         brand: '',
         description: ''
     }
 
-    console.log("initialValues: ", initialValues);
-    //form validation hook
-    const {data, errors, handleChange, handleSubmit} = useForm({
-        initialValues,
-        validations: {
+    const validations = {
             title:{
                 required: {
                     value: true,
                     message: 'نام کالا الزامی می باشد.'
+                }
+            },
+            model:{
+                required: {
+                    value: true,
+                    message: 'مدل الزامی می باشد.'
                 }
             },
             brand:{
@@ -59,14 +76,21 @@ const AddOrEditProductModal = ({neadEditProduct}) => {
                     message: 'نام برند الزامی می باشد.'
                 }
             }
-        }, 
+        }
+
+    //form validation hook
+    const {data, errors, handleChange, handleSubmit} = useForm({
+        initialValues,
+        validations, 
         onSubmit: () => handleSave() 
     })
-    console.log("data: ", data);
+
+    //--------------------------------------------------------------------------------------------//
+    //--------------------------------------------------------------------------------------------//
 
     //checkig which action needs to be dispach edit or add
     const handleSave = () => {
-        neadEditProduct ? dispatch(editAProduct(neadEditProduct.id, {...neadEditProduct, ...data, image})) : dispatch(addAProduct({...data, image}))
+        neadEditProduct ? dispatch(editAProduct(neadEditProduct.id, {...neadEditProduct, ...data, properties:{...details}, image})) : dispatch(addAProduct({...data, properties: {...details}, image}))
     }
 
     const handleTransformImageToBase64 = (e) => {
@@ -77,55 +101,100 @@ const AddOrEditProductModal = ({neadEditProduct}) => {
           setImage(res)
         })
     }
+    
+    
+    //open modal
+    const handleOpen = () => {
+        setOpenModal(true)
+    }
+    //close and save data if neaded
+    const handleClose = (data) => {
+        setOpenModal(false)
+        data && setDetails(data)
+    }
+
+    //render proper modal by category that user choose
+    const renderModalByCategory = () => {
+        switch (data['category']) {
+            case "گوشی موبایل":
+                return <PhoneDetails properties={neadEditProduct?.properties} save={handleClose}/>
+            case "لپ تاپ":
+                return <LaptopDetails properties={neadEditProduct?.properties} save={handleClose}/>
+            case "دوربین":
+                return <CameraDetails properties={neadEditProduct?.properties} save={handleClose}/>
+        
+            default:
+                break;
+        }
+    }
+    
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <InputBase 
-                label="تصویر" 
-                type='file'
-                onChange={e => handleTransformImageToBase64(e)}
-                style={{alignSelf: 'flex-start'}}
-            />
-            <TextField 
-                label="نام کالا" 
-                type='text'
-                required
-                variant="outlined" 
-                value={data['title'] || ''}
-                onChange={handleChange('title')}
-                error={Boolean(errors['title']) || false} 
-                helperText={errors['title'] || ''}
-                fullWidth={true} 
-                margin='dense'
-                autoFocus
-            />
-            <TextField
-                label="دسته بندی" 
-                variant="outlined" 
-                select
-                value={data['category'] || 'گوشی موبایل'}
-                onChange={handleChange('category')}
-                fullWidth={true} 
-                margin='dense'
-            >
-                {categories.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-            </TextField>
-            <TextField
-                label="برند" 
-                variant="outlined" 
-                value={data['brand'] || ''}
-                onChange={handleChange('brand')}
-                fullWidth={true} 
-                margin='dense'
-                error={Boolean(errors['brand']) || false} 
-                helperText={errors['brand'] || ''}
-            />
-            <Button type='submit' color="primary" variant='contained' >ذخیره</Button>
-        </Form>
+        <>
+            <Form onSubmit={handleSubmit} width={phones? '200px':'350px'}>
+                <InputBase 
+                    label="تصویر" 
+                    type='file'
+                    onChange={e => handleTransformImageToBase64(e)}
+                    style={{alignSelf: 'flex-start'}}
+                />
+                <TextField 
+                    label="نام کالا" 
+                    type='text'
+                    required
+                    variant="outlined" 
+                    value={data['title'] || ''}
+                    onChange={handleChange('title')}
+                    error={Boolean(errors['title']) || false} 
+                    helperText={errors['title'] || ''}
+                    fullWidth={true} 
+                    margin='dense'
+                    autoFocus
+                />
+                <TextField 
+                    label="مدل" 
+                    type='text'
+                    required
+                    variant="outlined" 
+                    value={data['model'] || ''}
+                    onChange={handleChange('model')}
+                    error={Boolean(errors['model']) || false} 
+                    helperText={errors['model'] || ''}
+                    fullWidth={true} 
+                    margin='dense'
+                />
+                <TextField
+                    label="دسته بندی" 
+                    variant="outlined" 
+                    select
+                    value={data['category'] || 'گوشی موبایل'}
+                    onChange={handleChange('category')}
+                    fullWidth={true} 
+                    margin='dense'
+                >
+                    {categories.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                </TextField>
+                <TextField
+                    label="برند" 
+                    variant="outlined" 
+                    value={data['brand'] || ''}
+                    onChange={handleChange('brand')}
+                    fullWidth={true} 
+                    margin='dense'
+                    error={Boolean(errors['brand']) || false} 
+                    helperText={errors['brand'] || ''}
+                />
+                <Button type='button' color="primary"  onClick={handleOpen} style={{alignSelf: 'flex-start', minWidth: '0px', margin: '8px 0'}}>افزودن جزئیات</Button>
+                <Button type='submit' color="primary" variant='contained' >ذخیره</Button>
+            </Form>
+            {openModal && <ModalContainer close={handleClose} isOpen={openModal}>
+                {renderModalByCategory()}
+            </ModalContainer>}
+        </>
     )
 }
 
