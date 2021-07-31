8d7132e434fc2e580b3f838c64790dc41df17d86
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-07-31 22:19:46 +0430

    replace navbar with new one

--- src/components/Layouts/AdminPanelLayout.jsx
+++ src/components/Layouts/AdminPanelLayout.jsx
@@ -1,44 +1,12 @@
 import React from 'react'
-import NavBar from '../NavBar';
+import Navbar from './AdminPanelNavbar';
 import PropTypes from 'prop-types'
-
-
-const navItems = [
-  {
-    to: '/adminPanel/products',
-    isRouterLink: true,
-    isButton: false,
-    content: 'محصولات',
-    props: {}
-  },
-  {
-    to: '/adminPanel/price&inventory',
-    isRouterLink: true,
-    isButton: false,
-    content: 'قیمت و موجودی',
-    props: {}
-  },
-  {
-    to: '/adminPanel/orders',
-    isRouterLink: true,
-    isButton: false,
-    content: 'سفارشات',
-    props: {}
-  },
-  {
-    to: '/',
-    isRouterLink: true,
-    isButton: true,
-    content: 'بازگشت به سایت',
-    props: {}
-  },
-]
 
 const AdminPanelLayout = ({children}) => {
 
     return (
         <div>
-            <NavBar title="پنل مدیریت فروشگاه" links={navItems}/>
+            <Navbar/>
             {children}
         </div>
     )
