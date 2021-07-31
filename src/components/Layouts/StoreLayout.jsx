e82a9de8be6b053ea7b252aef831affd8d63d2e6
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-07-31 22:44:39 +0430

    replace navbar with new one and add sidebar

--- src/components/Layouts/StoreLayout.jsx
+++ src/components/Layouts/StoreLayout.jsx
@@ -1,33 +1,28 @@
-import React from 'react'
-import NavBar from '../NavBar';
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import Sidebar from '../Sidebar'
+import { Box } from '@material-ui/core';
+import { useLocation } from 'react-router-dom';
+import StoreNavbar from './StoreNavbar';
 
-const navItems = [
-  {
-    to: '/adminPanel/products',
-    isRouterLink: true,
-    isButton: false,
-    content: 'مدیریت',
-    props: {}
-  },
-  {
-    to: '/card',
-    isRouterLink: true,
-    isButton: true,
-    content: 'سبد خرید',
-    props: {}
-  },
-]
 
 const StoreLayout = ({children}) => {
+  const [openSidebar, setOpenSidebar] = useState(false)
 
-    return (
-        <div>
-            <NavBar title="فروشگاه" links={navItems}/>
-            {children}
-            
-        </div>
-    )
+  const handleToggleSidebar = () => {
+    setOpenSidebar(!openSidebar)
+  }
+  const {pathname} = useLocation()
+  return (
+      <Box minHeight='100vh'>
+          <StoreNavbar toggleSidebar={handleToggleSidebar}/>
+          <Box component='main' display='flex' minHeight={`calc(100vh - 70px)`}>
+            { pathname === '/products' && <Sidebar toggleSidebar={openSidebar}/> }
+            { children }
+          </Box>
+          
+      </Box>
+  )
 }
 
 StoreLayout.propTypes = {
