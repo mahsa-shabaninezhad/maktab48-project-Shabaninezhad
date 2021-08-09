26bbc93cad3d42e50fd78f9d8fadbc69212c27b4
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-08-10 00:42:35 +0430

    add some media queries for typography and some overrides styles for datePicker

--- src/assets/material_ui/Theme.jsx
+++ src/assets/material_ui/Theme.jsx
@@ -21,8 +21,49 @@ let theme = createTheme({
       light: '#36382E',
       main: '#000'
     }
+  },
+  datePicker: {
+    selectColor: '#DADAD9'
+  },
+  overrides: {
+    MuiPickersToolbar: {
+      toolbar: { backgroundColor: '#DADAD9' }
+    },
+    MuiButton: {
+      textPrimary: {color: '#36382E'}
+    },
+    MuiPickersDay: {
+      daySelected : {backgroundColor: '#DADAD9'},
+      current: {color: '#1976d2'}
+    }
   }
 }, faIR);
+
+theme.typography.h1 = {
+  fontSize: '1.8rem',
+  fontWeight: 700,
+  
+  '@media (max-width:425px)': {
+    fontSize: '1.4rem',
+  },
+  '@media (min-width:700px)': {
+    fontSize: '2.2rem',
+  },
+};
+theme.typography.h2 = {
+  fontSize: '1.3rem',
+  fontWeight: 700,
+  '@media (min-width:600px)': {
+    fontSize: '1.5rem',
+  },
+};
+theme.typography.h3 = {
+  fontSize: '1.2rem',
+  fontWeight: 700,
+  '@media (min-width:600px)': {
+    fontSize: '1.3rem',
+  },
+};
 
 const Theme = ({children}) => {
     return (
