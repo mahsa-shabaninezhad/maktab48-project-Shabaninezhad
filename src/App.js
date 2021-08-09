a43803ba8ccab333c5878bdfef34477b7584b5f8
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-08-10 00:38:40 +0430

    add and modify some routes

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ import LandingPage from "./pages/store/LandingPage";
 import AdminPanelProductPage from "./pages/web/AdminPanelProductPage";
 import AdminPanelQuantityPage from "./pages/web/AdminPanelQuantityPage";
 import AdminPanelOrdersPage from "./pages/web/AdminPanelOrdersPage";
+import ModalContainer from "./components/modals/ModalContainer";
+import PaymentPage from "./pages/PaymentPage";
+import SuccessfulPayment from "./pages/SuccessfulPayment";
+import FailedPayment from "./pages/FailedPayment";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
-import ModalContainer from "./components/modals/ModalContainer";
 
 function App() {
 
@@ -19,11 +22,14 @@ function App() {
     <div className="App">
       <Switch>
         <StoreRoute exact path='/' component={LandingPage}/>
-        <StoreRoute exact path='/card' component={BasketPage}/>
+        <StoreRoute exact path='/cart' component={BasketPage}/>
         <StoreRoute exact path='/checkout' component={CheckoutPage}/>
-        <StoreRoute exact path='/product/:id' component={ProductPage}/>
-        <StoreRoute exact path='/products/:brand' component={ProductsPage}/>
+        <StoreRoute exact path='/product/:brand/:id' component={ProductPage}/>
+        <StoreRoute  path='/products' component={ProductsPage}/>
         <Route exact path="/login" component={LoginPage}/>
+        <Route exact path="/payment" component={PaymentPage}/>
+        <Route exact path='/payment/successful/:orderId' component={SuccessfulPayment}/>
+        <Route exact path='/payment/failed' component={FailedPayment}/>
         <AdminPanelRoute exact path="/adminPanel/products" component={AdminPanelProductPage}/>
         <AdminPanelRoute exact path="/adminPanel/price&inventory" component={AdminPanelQuantityPage}/>
         <AdminPanelRoute exact path="/adminPanel/orders" component={AdminPanelOrdersPage}/>
